92d8b1b4b46985345e83b8f23d2a675cadf68456
gayfulina <irinmdr@gmail.com>
2021-06-10 14:00:59 -0700

    organisation2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,34 @@
-import './Expenses.css'
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
-import Card from '../UI/Card'
-import ExpensesFilter from "./ExpensesFilter";
-import {useState} from "react";
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
 
-    const filterChangeHandler = selectedYear => {
+    const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
-    }
+    };
 
-    const filteredExpenses = props.items.filter(el => {
-        return el.date.getFullYear().toString() === filteredYear;
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
     });
-
 
     return (
         <div>
-            <Card className="expenses">
+            <Card className='expenses'>
                 <ExpensesFilter
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-               <ExpensesList items={filteredExpenses} />
+                <ExpensesChart expenses={filteredExpenses} />
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
-    )
-}
+    );
+};
 
 export default Expenses;
